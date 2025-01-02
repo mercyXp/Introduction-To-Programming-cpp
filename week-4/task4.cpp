a7f5971a449caeea7d4c92d55465f1a05e239999
MercyXp <munzenzimercy2024@gmail.com>
2025-01-02 17:54:26 +0700

    prime numbers between 1 and n

--- week-4/task4.cpp
+++ week-4/task4.cpp
@@ -6,6 +6,36 @@ int main() {
     int n;
     cin >> n;
 
+    bool isPrime;
+
+    for (int num = 2; num <= n; num++) {
+        isPrime = true;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << num << " ";
+        }
+    }
+
+    return 0;
+}
+
+
+/*  time limit exceeded 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
     for (int num = 2; num <= n; num++){
         int count = 0;
         for (int i = 1; i <= num; i++) {
@@ -19,3 +49,4 @@ int main() {
     }
     return 0;
 }
+*/
