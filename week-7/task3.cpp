6bb76a35ca3fffe5666aea632818ce40128f594f
MercyXp <munzenzimercy2024@gmail.com>
2025-01-10 03:01:17 +0700

    string to int

--- week-7/task3.cpp
+++ week-7/task3.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 int main() {
     string input;
-    cin >> input; 
-    
-    int result = 0; // Variable to store the converted integer
-    
-    // Convert the string to integer
-    for (char c : input) {
-        result = result * 10 + (c - '0');
-    }
-    
-    cout << result << endl; // Output the result
-    
+    cin >> input;
+
+    int number = stoi(input);
+
+    cout << number << endl;
+
     return 0;
 }
